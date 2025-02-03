4bc2f414e527f89cf753cd79031f2e800ac74d27
Thomas John Gough <Thomasyr@hotmail.com>
2025-02-03 10:39:04 +0000

    Add files via upload

--- twttr.py
+++ twttr.py
@@ -1,13 +1,20 @@
+import sys
+
 def main():
-    text = input("Input: ")#User types in string
-    print("", remove_v(text))#Print with the variable remove_v - 'v'
+    word = input("Input: ")  # User types in string
+    if not word.isalpha():  # Check if the input contains only alphabetic characters
+        sys.exit(1)  # Exit with status code 1 for invalid input
+    print(shorten(word))  # Print the shortened word with vowels removed
+    #Print with the variable remove_v - 'v'
 
-
-def remove_v(text): #def remove_v
+def shorten(word): #def remove_v
     v = "aeiouAEIOU" #v = all vowels
-    return "".join([char for char in text if char not in v]) 
+    return "".join([char for char in word if char not in v]) 
 #return the text(input) if the char not in v and join them together
 
+if __name__ == "__main__":
+    main()
 
-main()
+
+
 
